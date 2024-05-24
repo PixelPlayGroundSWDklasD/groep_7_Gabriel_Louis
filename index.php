b53ef6638757a128b725f0f9c30ddcd0e1987dbc
Louis Mpiakongo <144344057+6040040@users.noreply.github.com>
2024-05-24 09:11:30 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/4c1e45b6e3.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <title>Home</title>
+</head>
+<body>
+<style>
+body  {
+  background-image: url("images/Cyberpunk-City-Street-Scifi-Wallpaper-Graphics-74865888-1.png");
+}
+</style>
+<header>
+        <input type ="checkbox" name ="" id ="chk1">
+        <div class="logo"><h1>Pixel Playground</h1></div>
+            <div class="search-box">
+            </div>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Product</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Contact</a></li>
+                <a href="index.html"><li><i class="fa-solid fa-user"></i></a>
+                      
+                </li>
+            </ul>
+            <div class="menu">
+                <label for="chk1">
+                    <i class="fa fa-bars"></i>
+                </label>
+            </div>
+    </header>
+
+
+</body>
+</html>
