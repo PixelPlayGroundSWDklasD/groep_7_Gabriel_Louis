8c72ed79561f3a91134265c99514e73bee7088ce
Louis Mpiakongo <144344057+6040040@users.noreply.github.com>
2024-06-21 22:13:01 +0200

    kkk

--- galgje.php
+++ galgje.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hangman Game</title>
+    <meta name="author" content="Louis Mpiakongo">
     <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
